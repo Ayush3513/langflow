cf0683f985aee9b551a9268bd3e0f3f9f931061d
Ayush3513 <166975215+Ayush3513@users.noreply.github.com>
2025-01-19 10:48:13 +0530

    Update vite.config.ts 56

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
   resolve: {
@@ -10,28 +9,25 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  define: {
-    'process.env': {
-      VITE_LANGFLOW_BASE_URL: '/lf',
-      VITE_APPLICATION_TOKEN: "AstraCS:KHsgliOksMRwadpnlxhKIzsa:75da20a2a0a056d9e240102b4e0676c387b3dda1fa16419a6799b9ac59c8f430",
-      VITE_GROQ_API_KEY: "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"
-    }
-  },
   server: {
-    port: 5174,
-    proxy: {
+    proxy: mode === 'development' ? {
       '/lf': {
         target: 'https://api.langflow.astra.datastax.com',
         changeOrigin: true,
         secure: false,
-        rewrite: (path) => path.replace(/^\/lf/, '/lf'),
-        // Add the following line to allow access from the specified domain
-        configure: (proxy) => {
-          proxy.on('proxyReq', (proxyReq, req, res) => {
-            proxyReq.setHeader('Origin', 'https://langflow-flax.vercel.app');
+        headers: {
+          'Access-Control-Allow-Origin': 'https://langflow-flax.vercel.app/',
+          'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+          'Access-Control-Allow-Headers': 'Content-Type, Authorization'
+        },
+        configure: (proxy, _options) => {
+          proxy.on('proxyReq', (proxyReq, req, _res) => {
+            if (req.headers.authorization) {
+              proxyReq.setHeader('Authorization', req.headers.authorization);
+            }
           });
         }
       }
-    }
+    } : undefined
   }
 }));
